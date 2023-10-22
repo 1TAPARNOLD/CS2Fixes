9beb7e0c86c9a14390dda8ad8857067dab6b2c2a
blu133721 <tiganinpungasrl@gmail.com>
2023-10-22 15:33:28 +0300

    Update commands.cpp

--- src/commands.cpp
+++ src/commands.cpp
@@ -187,12 +187,6 @@ void ClientPrint(CBasePlayerController *player, int hud_dest, const char *msg, .
 	addresses::ClientPrint(player, hud_dest, buf, nullptr, nullptr, nullptr, nullptr);
 }
 
-void ClientSay(edict_t *pEntity, const char *pMsg, CBasePlayerController *player)
-{
-    char text[256];
-    sprintf(text, "[Player] %s: %s", player->GetPlayerName(), pMsg);
-    ClientPrintAll(HUD_PRINTTALK, text);
-}
 
 CON_COMMAND_CHAT(vipinfo, "vip info")
 {
