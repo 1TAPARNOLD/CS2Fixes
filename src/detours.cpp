0aec42496e6063a174d2b999703bd61230e2a6c3
blu133721 <tiganinpungasrl@gmail.com>
2023-10-23 22:40:07 +0300

    Update detours.cpp

--- src/detours.cpp
+++ src/detours.cpp
@@ -173,25 +173,30 @@ void FASTCALL Detour_UTIL_SayText2Filter(
 	int entindex = filter.GetRecipientIndex(0).Get() + 1;
 	CCSPlayerController *target = (CCSPlayerController *)g_pEntitySystem->GetBaseEntity((CEntityIndex)entindex);
 
-	int iCommandPlayer = player->GetPlayerSlot();
+	     int iCommandPlayer = pEntity->GetPlayerSlot();
 
-	ZEPlayer *pPlayer = g_playerManager->GetPlayer(iCommandPlayer);
-
+    ZEPlayer *pPlayer = g_playerManager->GetPlayer(iCommandPlayer);
+	
 	char sBuffer[256];
-	if (!pPlayer->IsAdminFlagSet(ADMFLAG_BAN))
-	{
-		V_snprintf(sBuffer, sizeof(sBuffer), " \4[ADMIN]\3 %s: \1%s", param1, param2);
-		Message("DEBUG: msg_name: %s, param1: %s, param2: %s, param3: %s, param4: %s\n", msg_name, param1, param2, param3, param4);
-		UTIL_SayTextFilter(filter, sBuffer, pEntity, eMessageType);
-	}
-
-	if (!pPlayer->IsAdminFlagSet(ADMFLAG_BAN))
-	{
-		V_snprintf(sBuffer, sizeof(sBuffer), " \4[Player]\3 %s: \1%s", param1, param2);
-		Message("DEBUG: msg_name: %s, param1: %s, param2: %s, param3: %s, param4: %s\n", msg_name, param1, param2, param3, param4);
-		UTIL_SayTextFilter(filter, sBuffer, pEntity, eMessageType);
-	}
-
+	if (pPlayer->IsAdminFlagSet(ADMFLAG_RESERVATION))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\4HELPER]\1 %s: \1%s", param1, param2);
+	if (pPlayer->IsAdminFlagSet(ADMFLAG_GENERIC))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\13ADMINISTRATOR]\1 %s: \1%s", param1, param2);
+	if (pPlayer->IsAdminFlagSet(ADMFLAG_KICK))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\12MODERATOR]\1 %s: \1%s", param1, param2);
+	if (pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\2VETERAN]\1 %s: \1%s", param1, param2);
+	if (pPlayer->IsAdminFlagSet(ADMFLAG_VOTE))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\2MANAGER]\1 %s: \1%s", param1, param2);
+	if (pPlayer->IsAdminFlagSet(ADMFLAG_CHANGEMAP))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\2CO-OWNER]\1 %s: \1%s", param1, param2);
+		if (pPlayer->IsAdminFlagSet(ADMFLAG_ROOT))
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \1[\2OWNER]\1 %s: \1%s", param1, param2);
+	else
+	    V_snprintf(sBuffer, sizeof(sBuffer), " \4[Player]\3 %s: \1%s", param1, param2);
+	
+	Message("DEBUG: msg_name: %s, param1: %s, param2: %s, param3: %s, param4: %s\n", msg_name, param1, param2, param3, param4);
+	UTIL_SayTextFilter(filter, sBuffer, pEntity, eMessageType);
 }
 
 
