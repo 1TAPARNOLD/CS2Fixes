caf10ee463a70a362c07b786d87d0c5b206927b3
blu133721 <tiganinpungasrl@gmail.com>
2023-10-25 18:14:37 +0300

    Update detours.cpp

--- src/detours.cpp
+++ src/detours.cpp
@@ -178,23 +178,23 @@ void FASTCALL Detour_UTIL_SayText2Filter(
     ZEPlayer *pPlayer = g_playerManager->GetPlayer(iCommandPlayer);
 	
 		char sBuffer[256];
-        if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM2))
+        if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM1)) // p
         {
             V_snprintf(sBuffer, sizeof(sBuffer), " \1[\13HELPER\1] \10%s: \4%s", param1, param2);
         }
-        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM3))
+        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM2)) // q
         {
             V_snprintf(sBuffer, sizeof(sBuffer), " \1[\14ADMINISTRATOR\1] \10%s: \4%s", param1, param2);
         }
-        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM4))
+        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM3)) // r
         {
             V_snprintf(sBuffer, sizeof(sBuffer), " \1[\4MODERATOR\1]\10 %s: \4%s", param1, param2);
         }
-        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM5))
+        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM4)) // s
         {
             V_snprintf(sBuffer, sizeof(sBuffer), " \1[\7VETERAN\1]\10 %s: \4%s", param1, param2);
         }
-        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM6))
+        else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM5)) // t
         {
             V_snprintf(sBuffer, sizeof(sBuffer), " \1[\7MANAGER\1]\10 %s: \4%s", param1, param2);
         }
