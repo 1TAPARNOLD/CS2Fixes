f4c3371193203db31f96ad29dfa40ea8fd4cd117
Trofin Razvan <77455545+1TAPARNOLD@users.noreply.github.com>
2023-10-27 23:22:30 +0300

    Update detours.cpp
    Rezolvat tag-uri

--- src/detours.cpp
+++ src/detours.cpp
@@ -196,15 +196,15 @@ void FASTCALL Detour_UTIL_SayText2Filter(
         }
 		else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM7)) // u
         {
-            V_snprintf(sBuffer, sizeof(sBuffer), " \1[\7TESTER\1]\10 %s: \4%s", param1, param2);
+            V_snprintf(sBuffer, sizeof(sBuffer), " \1[\11TESTER\1]\10 %s: \4%s", param1, param2);
         }
-		else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM4)) // r
+		else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM5)) // s
         {
-            V_snprintf(sBuffer, sizeof(sBuffer), " \1[\7VETERAN\1]\10 %s: \4%s", param1, param2);
+            V_snprintf(sBuffer, sizeof(sBuffer), " \1[\7MANAGER\1]\10 %s: \4%s", param1, param2);
         }
         else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM8)) // v
         {
-            V_snprintf(sBuffer, sizeof(sBuffer), " \1[\7SUPERVIZOR\1]\10 %s: \4%s", param1, param2);
+            V_snprintf(sBuffer, sizeof(sBuffer), " \1[\20SUPERVIZOR\1]\10 %s: \4%s", param1, param2);
         }
         else if (pPlayer->IsAdminFlagSet(ADMFLAG_CUSTOM6)) // t
         {
